61cfe3b30694ee5b84862b2ff4dffca7c64d4608
gianluca-gradogna <gianluca.gradogna@mono-grid.com>
2023-09-05 16:08:19 +0200

    update class name

--- vite.config.js
+++ vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
       output: {
         format: 'umd',
         entryFileNames: 'main.js',
-        esModule: false,
+        esModule: true,
         compact: true,
         globals: {
           jquery: '$',
